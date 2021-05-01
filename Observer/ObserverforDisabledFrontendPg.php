732d169bd13f92591e7f74526c0c62c600de91e7
Gustavo Ulyssea <gustavo.ulyssea@gmail.com>
2021-04-30 21:32:52 -0300

    disable method observer

---
+++ Observer/ObserverforDisabledFrontendPg.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @author Gustavo Ulyssea - gustavo.ulyssea@gmail.com
+ * @copyright Copyright (c) 2020-2021 GumNet (https://gum.net.br)
+ * @package GumNet AME
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GUM Net (https://gum.net.br). AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace AditumPayment\Magento2\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\App\Request\DataPersistorInterface;
+use Magento\Framework\App\ObjectManager;
+
+class ObserverforDisabledFrontendPg implements ObserverInterface
+{
+    protected $_appState;
+    protected $scopeConfig;
+
+    public function __construct(
+        \Magento\Framework\App\State $appState,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    )
+    {
+        $this->_appState = $appState;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $result = $observer->getEvent()->getResult();
+        $method_instance = $observer->getEvent()->getMethodInstance();
+        $quote = $observer->getEvent()->getQuote();
+        if ($method_instance->getCode() == 'aditumcc'
+            &&!$this->scopeConfig->getValue("payment/aditum_cc/enable")) {
+            $result->setData('is_available', false);
+        }
+        if ($method_instance->getCode() == 'aditumboleto'
+            &&!$this->scopeConfig->getValue("payment/aditum_boleto/enable")) {
+            $result->setData('is_available', false);
+        }
+    }
+    protected function getDisableAreas()
+    {
+        return array(\Magento\Framework\App\Area::AREA_FRONTEND, \Magento\Framework\App\Area::AREA_WEBAPI_REST);
+    }
+}
